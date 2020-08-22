cdf6f529976941cd09b5d8ff2d84246eadd3ad75
Thomas Whiteley <tom.whiteley@gmail.com>
2020-08-22 16:47:04 +0000

    Trying to get repl.it to run....

--- app.js
+++ app.js
@@ -2,10 +2,13 @@ const express = require('express')
 const app = express()
 const port = 3000
 
-app.get('/', (req, res) => {
+console.log("OK")
+
+/* app.get('/', (req, res) => {
   res.send('Hello World!')
 })
 
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`)
 })
+*/
